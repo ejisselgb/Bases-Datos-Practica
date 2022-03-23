7f0cee2a0d79595b3bf02409f84c55812db5af82
Sebasarias17 <sebasarias33314@gmail.com>
2022-03-22 22:09:35 -0500

    Inserts (Usuario,Peliculas,Genero)

--- TrabajosEnClase/TrabajoClase.sql
+++ TrabajosEnClase/TrabajoClase.sql
@@ -12,7 +12,8 @@ CREATE TABLE "Grupo 4"."Usuario"(
 CREATE TABLE "Grupo 4"."Peliculas"(
 	id_pelicula serial, 
 	nombre_pelicula varchar(40), 
-	portada varchar(100), restriccion_edad int2, 
+	portada varchar(100), 
+	restriccion_edad int2, 
 	duracion time, 
 	fecha_estreno date, 
 	link_pelicula varchar(50)
@@ -47,3 +48,17 @@ ALTER TABLE "Grupo 4"."Genero"
 	ADD CONSTRAINT pk_genero PRIMARY KEY(id_genero),
 	ADD CONSTRAINT nombre_genero_uq UNIQUE (nombre_genero),
 	ALTER COLUMN nombre_genero SET NOT NULL;
+
+SET DATESTYLE TO "EUROPEAN";
+
+INSERT INTO "Grupo 4"."Usuario" VALUES ('Giansz','Imagen.JPG','3022205116',18,'Gian Paul','Sanchez','1q2w3e4r5t','Giansz@gmail.com');
+INSERT INTO "Grupo 4"."Usuario" VALUES ('JuanesJara','Imagen.JPG','3032505134',18,'Juan Esteban','Jaramillo','JuanesLindo','jarami@gmail.com');
+INSERT INTO "Grupo 4"."Usuario" VALUES ('PalacioJ','Imagen.JPG','3034405547',18,'Samuel','Palacio','Zetlix123456','SpalacioJ@gmail.com');
+
+INSERT INTO "Grupo 4"."Peliculas" values(1,'El paseo 2','Portada.JPG',15,'1:05:50','23/3/2022','https://elpaseo-mp4.com');
+INSERT INTO "Grupo 4"."Peliculas" values(2,'A todo gas','Portada2.JPG',18,'2:15:30','22/3/2012','https://a-todo-gas-mp4.com');
+INSERT INTO "Grupo 4"."Peliculas" values(3,'Lobezno','Portada3.JPG',10,'5:35:27','30/3/2017','https://lobezno-mp4.com');
+
+INSERT INTO "Grupo 4"."Genero" values (01,'Comedia');
+INSERT INTO "Grupo 4"."Genero" values (02,'Accion');
+INSERT INTO "Grupo 4"."Genero" values (03,'Drama');
