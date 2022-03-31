ac385d5b8c98c4a142590f2c625ad4ca86cb8b55
GianSz <giansz2003@gmail.com>
2022-03-30 19:02:26 -0500

    Código back-end peticiones

--- peliculas_maqueta/Servidor/controllers/index.js
+++ peliculas_maqueta/Servidor/controllers/index.js
@@ -24,36 +24,65 @@ router.get('/api', function (req, res, next) {
 })
 
 router.post('/api/user', function (req, res, next) {
-    const { username, password } = req.body
-    moviesModel.getUser(username, password).then(user=>{
+    const { nombre_usuario, contrasena } = req.body
+    moviesModel.getUser(nombre_usuario, contrasena).then(user=>{
         res.status(200).send(user.rows)
     }).catch(err => {
         console.log(err)
-        return res.status(500).send('Error getting user')
+        return res.status(500).send('Error getting user '+nombre_usuario)
+    })
+})
+
+router.get('/api/allUsers', function (req, res, next) {
+    moviesModel.getAllUsers().then(users=>{
+        res.status(200).send(users.rows)
+    }).catch(err => {
+        console.log(err)
+        return res.status(500).send('Error getting users')
     })
 })
 
 router.post('/api/user/create', function (req, res, next) {
-    const { username, password } = req.body
-    moviesModel.insertUser(username, password).then(user=>{
-        res.status(200).send({message: 'insert data', rowCount: user.rowCount})
+    const { nombre_usuario,imagen, telefono, edad, nombre, apellido,contrasena,correo } = req.body
+    moviesModel.insertUser(nombre_usuario,imagen,telefono, edad, nombre, apellido,contrasena,correo).then(user=>{
+        res.status(200).send({message: 'create user '+nombre_usuario, rowCount: user.rowCount})
     }).catch(err => {
         console.log(err)
-        return res.status(500).send('Error getting user')
+        return res.status(500).send('Error creating user')
+    })
+})
+
+router.post('/api/user/update', function (req, res, next) {
+    const { nombre_usuario, contrasena } = req.body
+    moviesModel.updateUser(nombre_usuario, contrasena).then(user=>{
+        res.status(200).send({message: 'update data user '+nombre_usuario, rowCount: user.rowCount})
+    }).catch(err => {
+        console.log(err)
+        return res.status(500).send('Error updating user ')
+    })
+})
+
+router.post('/api/user/delete', function (req, res, next) {
+    const { nombre_usuario } = req.body
+    moviesModel.deleteUser(nombre_usuario).then(user=>{
+        res.status(200).send({message: 'delete user '+nombre_usuario, rowCount: user.rowCount})
+    }).catch(err => {
+        console.log(err)
+        return res.status(500).send('Error deleting user '+nombre_usuario)
     })
 })
 
 router.get('/api/movie/:id', function (req, res, next) {
     const id_pelicula = req.params.id
-    moviesModel.getMovieId(id_pelicula).then(movie=>{
+    moviesModel.getMovie(id_pelicula).then(movie=>{
         res.status(200).send(movie.rows)
     }).catch(err => {
         console.log(err)
-        return res.status(500).send(`Error getting movie '${id_pelicula}'`)
+        return res.status(500).send('Error getting movie '+id_pelicula)
     })
 })
 
-router.get('/api/getAllMovies', function (req, res, next) {
+router.get('/api/allMovies', function (req, res, next) {
     moviesModel.getAllMovies().then(movies=>{
         res.status(200).send(movies.rows)
     }).catch(err => {
@@ -62,13 +91,33 @@ router.get('/api/getAllMovies', function (req, res, next) {
     })
 })
 
+router.post('/api/movie/create', function (req, res, next) {
+    const { id_pelicula,nombre_pelicula,portada,restriccion_edad,duracion,fecha_estreno,link_pelicula} = req.body
+    moviesModel.insertMovie(id_pelicula,nombre_pelicula,portada,restriccion_edad,duracion,fecha_estreno,link_pelicula).then(movie=>{
+        res.status(200).send({message: 'create movie '+id_pelicula, rowCount: movie.rowCount})
+    }).catch(err => {
+        console.log(err)
+        return res.status(500).send('Error creating movie')
+    })
+})
+
 router.post('/api/movie/update', function(req,res,next){
     const {id_pelicula, portada} = req.body
     moviesModel.uptadeMovie(id_pelicula, portada).then(movie=>{
-        res.status(200).send(movie.rows)
+        res.status(200).send({message: 'update movie '+id_pelicula, rowCount: movie.rowCount})
     }).catch(err=>{
         console.log(err)
-        return res.status(500).send(`Error updating movie '${req.params.id}'`)
+        return res.status(500).send('Error updating movie '+id_pelicula)
+    })
+})
+
+router.post('/api/movie/delete', function (req, res, next) {
+    const { id_pelicula } = req.body
+    moviesModel.deleteMovie(id_pelicula).then(movie=>{
+        res.status(200).send({message: 'delete movie '+id_pelicula, rowCount: movie.rowCount})
+    }).catch(err => {
+        console.log(err)
+        return res.status(500).send('Error deleting movie '+id_pelicula)
     })
 })
 
--- peliculas_maqueta/Servidor/models/index.js
+++ peliculas_maqueta/Servidor/models/index.js
@@ -3,45 +3,65 @@ const connection = require('../config/connection')
 
 module.exports = {
 
+    async getMovie(id_pelicula) {
+        const querySelect = `SELECT * FROM "Grupo 4"."Peliculas" WHERE id_pelicula='${id_pelicula}';`
+        const result = await connection.query(querySelect)
+        console.log(result.rows, "xd")
+        return result
+    },
+
     async getAllMovies() {
         const querySelect = `SELECT * FROM "Grupo 4"."Peliculas";`
         const result = await connection.query(querySelect)
         return result
     },
 
-    async getMovieId(id_pelicula) {
-        const querySelect = `SELECT FROM "Grupo 4"."Peliculas" WHERE id_pelicula='${id_pelicula};`
-        const result = await connection.query(querySelect)
-        return result
-    },
-
-    async uptadeMovie(id_pelicula, portada){
-        const queryUpdate = `UPDATE "Grupo 4"."Peliculas" SET portada='${portada}' WHERE id_pelicula='${id_pelicula}'`
-        const result = await connection.query(queryUpdate)
-        return result
-    },
-
-    async getUser(username, password) {
-        const querySelect = `SELECT FROM "Grupo 4"."Usuario" WHERE nombre_usuario='${username}' and contrasena'${password}';`
-        const result = await connection.query(querySelect)
-        return result
-    },
-
-    async insertUser(username, password){
-        const queryInsert = `'${username}' and '${password}';`
+    async insertMovie(id_pelicula, nombre_pelicula, portada, restriccion_edad, duracion, fecha_estreno, link_pelicula){
+        const queryInsert = `INSERT INTO "Grupo 4"."Peliculas" VALUES('${id_pelicula}', '${nombre_pelicula}', '${portada}', '${restriccion_edad}', '${duracion}', '${fecha_estreno}', '${link_pelicula}');`
         const result = await connection.query(queryInsert)
         return result
     },
 
-    async updateUser(username, newPassword){
-        const queryUpdate = `'${newPassword}' and '${username}';`
+    async uptadeMovie(id_pelicula, portada){
+        const queryUpdate = `UPDATE "Grupo 4"."Peliculas" SET portada='${portada}' WHERE id_pelicula='${id_pelicula}';`
         const result = await connection.query(queryUpdate)
         return result
     },
 
-    async deleteUser(username){
-        const queryDelete = `'${username}';`
+    async deleteMovie(id_pelicula){
+        const queryDelete = `DELETE FROM "Grupo 4"."Peliculas" WHERE id_pelicula='${id_pelicula}';`
         const result = await connection.query(queryDelete)
         return result
-    }
+    },
+
+    async getUser(nombre_usuario, contrasena) {
+        const querySelect = `SELECT * FROM "Grupo 4"."Usuario" WHERE nombre_usuario='${nombre_usuario}' AND contrasena='${contrasena}';`
+        const result = await connection.query(querySelect)
+        console.log(result.rows, "xd")
+        return result
+    },
+
+    async getAllUsers(){
+        const querySelect = `SELECT * FROM "Grupo 4"."Usuario";`
+        const result = await connection.query(querySelect)
+        return result
+    },
+
+    async insertUser(nombre_usuario,imagen, telefono, edad, nombre, apellido,contrasena,correo){
+        const queryInsert = `INSERT INTO "Grupo 4"."Usuario" VALUES('${nombre_usuario}', '${imagen}', '${telefono}', '${edad}', '${nombre}', '${apellido}', '${contrasena}', '${correo}');`
+        const result = await connection.query(queryInsert)
+        return result
+    },
+
+    async updateUser(nombre_usuario, newContrasena){
+        const queryUpdate = `UPDATE "Grupo 4"."Usuario" SET contrasena='${newContrasena}' WHERE nombre_usuario='${nombre_usuario}';`
+        const result = await connection.query(queryUpdate)
+        return result
+    },
+
+    async deleteUser(nombre_usuario){
+        const queryDelete = `DELETE FROM "Grupo 4"."Usuario" WHERE nombre_usuario='${nombre_usuario}';`
+        const result = await connection.query(queryDelete)
+        return result
+    }   
 }
