878fca40004ee8db7eb9092de33bbabc959db41a
Sebasarias17 <sebasarias33314@gmail.com>
2022-06-13 22:12:34 -0500

    Push trabajo # 3

--- Trabajo 3/conexion.js
+++ Trabajo 3/conexion.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/MOVIES-EAFIT', {
+mongoose.connect('', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
